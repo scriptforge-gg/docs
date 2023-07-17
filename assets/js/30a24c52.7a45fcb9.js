4f84f154dc9e65b54b936dd5add7211093f1f9de
Mateq <mateqjs@gmail.com>
2023-07-17 23:52:50 +0200

    update

---
+++ assets/js/30a24c52.7a45fcb9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksfggv_78282=self.webpackChunksfggv_78282||[]).push([[453],{8605:l=>{l.exports=JSON.parse('{"label":"hello","permalink":"/blog/tags/hello","allTagsPath":"/blog/tags","count":2}')}}]);
